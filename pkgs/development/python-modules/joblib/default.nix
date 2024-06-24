f294a1ab990396451ab8ec0198eec6b48d1f2d97
Danil Suetin <suetin085@protonmail.com>
2024-06-25 01:22:37 +0200

    python3Packages.joblib: 1.4.0 -> 1.4.2

--- pkgs/development/python-modules/joblib/default.nix
+++ pkgs/development/python-modules/joblib/default.nix
@@ -2,7 +2,7 @@
   lib,
   buildPythonPackage,
   pythonOlder,
-  fetchPypi,
+  fetchFromGitHub,
   pythonAtLeast,
   stdenv,
 
@@ -21,14 +21,16 @@
 
 buildPythonPackage rec {
   pname = "joblib";
-  version = "1.4.0";
+  version = "1.4.2";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-HrDcCRkZzThEkN6JDLXf1ThBCm1LO1Tu8J+4xQtAmxw=";
+  src = fetchFromGitHub {
+    owner = "joblib";
+    repo = "joblib";
+    rev = version;
+    hash = "sha256-/II5E97u7UkfQ87DU+TWQ2IdsmQf24QRCfgSwUtgSBI=";
   };
 
   nativeBuildInputs = [ setuptools ];
@@ -65,6 +67,6 @@ buildPythonPackage rec {
     description = "Lightweight pipelining: using Python functions as pipeline jobs";
     homepage = "https://joblib.readthedocs.io/";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ dsuetin ];
   };
 }
